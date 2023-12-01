4e841d0bd15ec85bf57457a909ea4cebeb076522
Scotti Scholter Marichal <scscholt@mail.uni-mannheim.de>
2023-12-01 18:26:06 +0100

    localhost

--- PAI5/Server/src/Main.java
+++ PAI5/Server/src/Main.java
@@ -2,7 +2,7 @@ import java.io.DataInputStream;
 import java.net.*;
 
 public class Main {
-    String clientIpAddress = "127.0.0.1";
+    private static String clientIpAddress = "127.0.0.1";
     public static void main(String[] args) {
 
         System.out.println("Server started...\nWaiting for connection...");
