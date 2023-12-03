90904abc73e7c8580d047baaeb053a272a24186b
Scotti Scholter Marichal <scscholt@mail.uni-mannheim.de>
2023-12-03 15:48:32 +0100

    mensaje de string

--- PAI5/MyApplication/app/src/main/java/com/example/myapplication/Message.java
+++ PAI5/MyApplication/app/src/main/java/com/example/myapplication/Message.java
@@ -16,7 +16,10 @@ public class Message implements Serializable{
         this.sillones = sillones;
         this.id = id;
     }
-
+    public static Message getMessageFromString(String message) {
+        String[] values = message.split(";");
+        return new Message(Integer.parseInt(values[0]), Integer.parseInt(values[1]), Integer.parseInt(values[2]), Integer.parseInt(values[3]), Integer.parseInt(values[4]));
+    }
     public int getCamas() {
         return camas;
     }
